d1f1a31f56d669521e08f22f0bb9d1557b9fcb71
pubparksy <pubparksy@gmail.com>
2025-01-16 04:46:40 +0900

    docs: Add 백준 정답 제출용 Template java 파일

---
+++ challenge/src/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//        int input = br.readLine(); // read()는 개행X
+
+
+
+
+        // 결과 출력
+//        bw.write(String.valueOf(input));
+//        bw.newLine(); // 개행
+
+        // 자원 닫기
+//        br.close();
+//        bw.close();
+    }
+}
+
