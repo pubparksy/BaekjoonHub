ae08ae97b6dd1d051debbc58c990be2433f2dc5d
pubparksy <pubparksy@gmail.com>
2025-02-13 19:03:21 +0900

    [Silver IV] Title: 트리 만들기, Time: 104 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Silver/14244. 트리 만들기/트리 만들기.java
+++ 백준/Silver/14244. 트리 만들기/트리 만들기.java
@@ -8,25 +8,23 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken()); // 노드 개수
+        int m = Integer.parseInt(st.nextToken()); // 간선 개수
 
-        int count = 0;
-        int node = 0;
-        if(m == 2){
-            count = 1;
-        }
+        int count = 0; // 현재 사용한 간선 개수
+        int node = 0;  // 마지막으로 연결된 노드
 
-        for(int i=1; i<n; i++){
-            if(m > count) {
-                sb.append("0").append(" ").append(i).append("\n");
+        // 루트(0)에서 M개의 자식 노드를 만듦
+        for (int i = 1; i < n; i++) {
+            if (count < m) { 
+                sb.append("0 ").append(i).append("\n"); // 루트 노드(0)와 연결
                 count++;
             } else {
-                sb.append(node).append(" ").append(i).append("\n");
+                sb.append(node).append(" ").append(i).append("\n"); // 나머지는 직렬 연결
             }
             node = i;
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
